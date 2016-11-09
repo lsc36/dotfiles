1ea83ebb073c401cb387cd9d1e7632d4993cf220
Shao-Chuan Lee <lsc@lv6.tw>
2016-11-09 17:43:36 +0900

    init

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+repo_root=`realpath $(dirname $0)`
+
+git_prompt_sh=$repo_root/dot/git-prompt.sh
+[ ! -f "$git_prompt_sh" ] && curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > $git_prompt_sh
+
+for i in `ls $repo_root/dot`; do
+	src=$repo_root/dot/$i
+	dst=~/.$i
+	[ -h "$dst" ] && rm $dst
+	[ -f "$dst" ] && echo "Moving $dst to $dst.old" && mv $dst $dst.old
+	echo "Symlink $dst -> $src"
+	ln -s $src $dst
+done
