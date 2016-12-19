83ab43578a3fb84847e08a9c2f216d85dcb5cab2
Shao-Chuan Lee <lsc@lv6.tw>
2016-12-19 17:22:53 +0800

    vimrc, download vim-plug when install

--- install.sh
+++ install.sh
@@ -7,6 +7,12 @@ if [ ! -f "$git_prompt_sh" ]; then
 	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > $git_prompt_sh
 fi
 
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+	echo "Downloading vim-plug"
+	mkdir -p ~/.vim/autoload
+	curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > ~/.vim/autoload/plug.vim
+fi
+
 for i in `ls $repo_root/dot`; do
 	src=$repo_root/dot/$i
 	dst=~/.$i
@@ -15,3 +21,6 @@ for i in `ls $repo_root/dot`; do
 	echo "Symlink $dst -> $src"
 	ln -s $src $dst
 done
+
+echo "Installing vim plugins"
+vim -c PlugUpdate
